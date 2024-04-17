177e24a30ac3a2802e747cb6b9ed3d52a720e0dc
anjalitanwar946 <165695150+anjalitanwar946@users.noreply.github.com>
2024-04-17 17:33:34 +0000

    ice cream import

---
+++ ice_cream.py
@@ -0,0 +1,9 @@
+def add_topping():
+    toppings= ["chocolate syrup", "sprinkles", "hot fudge", "whipped cream", "crushed nuts"]
+    scoop_size=["large","small"]
+    return toppings, scoop_size
+
+def make_shake():
+   
+    shake = ["nut", "fruit"]
+    return shake
---
+++ problem4import.py
@@ -0,0 +1,7 @@
+import ice_cream
+toppings, scoop_sizes = ice_cream.add_topping()
+print("toppings:", toppings)
+print(" scoop sizes:", scoop_sizes)
+
+shake= ice_cream.make_shake()
+print("shake :", shake)
