ed69ceda813c25802725736c3f19c880b642173f
anjalitanwar946 <165695150+anjalitanwar946@users.noreply.github.com>
2024-04-17 15:40:01 +0000

    1st commit

---
+++ problem1.py
@@ -0,0 +1,13 @@
+def Get_name():
+    name = input(name: )
+    return name
+
+def get_tshirt():
+    brands=["Prada","LV","Gucci","Zudio","Balanciaga"]
+    user=Get_name()
+    brand_name= input("T-shirt brand you are looking for: ")
+    if brand_name in brands :
+     print(f"Hi {user},Unfortunately the brand you are looking for is available.")
+    else:
+     print(f"Hi {user},Unfortunately the brand you are looking for is un available.")
+get_tshirt()
